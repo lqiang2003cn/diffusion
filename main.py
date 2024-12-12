9d17f0a0a4515069f21836e2b01aaa8ceabe07ed
lqiang2003cn <randycoq@163.com>
2024-12-12 21:42:15 +0800

    d

---
+++ main.py
@@ -0,0 +1,120 @@
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import pandas as pd
+import numpy as np
+import torch
+import seaborn as sns
+import itertools
+from tqdm.auto import tqdm
+
+data_distribution = torch.distributions.mixture_same_family.MixtureSameFamily(
+    torch.distributions.Categorical(torch.tensor([1, 2])),
+    torch.distributions.Normal(torch.tensor([-4., 4.]), torch.tensor([1., 1.]))
+)
+
+dataset = data_distribution.sample(torch.Size([1000, 1]))
+# sns.histplot(dataset[:, 0])
+# plt.show()
+
+# we will keep these parameters fixed throughout
+TIME_STEPS = 250
+BETA = 0.02
+
+
+def do_diffusion(data, steps=TIME_STEPS, beta=BETA):
+    # perform diffusion following equation 2
+    # returns a list of q(x(t)) and x(t)
+    # starting from t=0 (i.e., the dataset)
+
+    distributions, samples = [None], [data]
+    xt = data
+    for t in range(steps):
+        q = torch.distributions.Normal(
+            np.sqrt(1 - beta) * xt,
+            np.sqrt(beta)
+        )
+        xt = q.sample()
+
+        distributions.append(q)
+        samples.append(xt)
+
+    return distributions, samples
+
+
+ds, samples = do_diffusion(dataset)
+
+for t in torch.stack(samples)[:, :, 0].T[:100]:
+    plt.plot(t, c='navy', alpha=0.1)
+plt.xlabel('Diffusion time')
+plt.ylabel('Data')
+plt.show()
+
+
+def compute_loss(forward_distributions, forward_samples, mean_model, var_model):
+    # here we compute the loss in equation 3
+    # forward = q , reverse = p
+
+    # loss for x(T)
+    p = torch.distributions.Normal(
+        torch.zeros(forward_samples[0].shape),
+        torch.ones(forward_samples[0].shape)
+    )
+    loss = -p.log_prob(forward_samples[-1]).mean()
+
+    for t in range(1, len(forward_samples)):
+        xt = forward_samples[t]  # x(t)
+        xprev = forward_samples[t - 1]  # x(t-1)
+        q = forward_distributions[t]  # q( x(t) | x(t-1) )
+
+        # normalize t between 0 and 1 and add it as a new column
+        # to the inputs of the mu and sigma networks
+        xin = torch.cat(
+            (xt, (t / len(forward_samples)) * torch.ones(xt.shape[0], 1)),
+            dim=1
+        )
+        # compute p( x(t-1) | x(t) ) as equation 1
+        mu = mean_model(xin)
+        sigma = var_model(xin)
+        p = torch.distributions.Normal(mu, sigma)
+
+        # add a term to the loss
+        loss -= torch.mean(p.log_prob(xprev))
+        loss += torch.mean(q.log_prob(xt))
+
+    return loss / len(forward_samples)
+
+
+mean_model = torch.nn.Sequential(
+    torch.nn.Linear(2, 4), torch.nn.ReLU(),
+    torch.nn.Linear(4, 1)
+)
+var_model = torch.nn.Sequential(
+    torch.nn.Linear(2, 4), torch.nn.ReLU(),
+    torch.nn.Linear(4, 1), torch.nn.Softplus()
+)
+
+optim = torch.optim.AdamW(
+    itertools.chain(mean_model.parameters(), var_model.parameters()),
+    lr=1e-2, weight_decay=1e-6,
+)
+
+loss_history = []
+bar = tqdm(range(1000))
+for e in bar:
+    forward_distributions, forward_samples = do_diffusion(dataset)
+
+    optim.zero_grad()
+    loss = compute_loss(
+        forward_distributions, forward_samples, mean_model, var_model
+    )
+    loss.backward()
+    optim.step()
+
+    bar.set_description(f'Loss: {loss.item():.4f}')
+    loss_history.append(loss.item())
+
+plt.plot(loss_history)
+plt.yscale('log')
+plt.ylabel('Loss')
+plt.xlabel('Training step')
+plt.show()

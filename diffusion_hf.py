6761ce4e00585b18d80cc069513de072211cc8e3
lqiang2003cn <randycoq@163.com>
2024-12-13 02:56:51 +0000

    i

--- diffusion_hf.py
+++ diffusion_hf.py
@@ -17,26 +17,153 @@ print("Input shape:", x.shape)
 print("Labels:", y)
 
 
-plt.imshow(torchvision.utils.make_grid(x)[0], cmap="Greys")
-plt.show()
+# plt.imshow(torchvision.utils.make_grid(x)[0], cmap="Greys")
+# plt.show()
 
 def corrupt(x, amount):
     """Corrupt the input `x` by mixing it with noise according to `amount`"""
     noise = torch.rand_like(x)
     amount = amount.view(-1, 1, 1, 1)  # Sort shape so broadcasting works
-    return x * (1 - amount) # + noise * amount
+    return x * (1 - amount)  + noise * amount
+#
+# # Plotting the input data
+# fig, axs = plt.subplots(2, 1, figsize=(12, 5))
+# axs[0].set_title("Input data")
+# axs[0].imshow(torchvision.utils.make_grid(x)[0], cmap="Greys")
+# # Adding noise
+# amount = torch.linspace(0, 1, x.shape[0])  # Left to right -> more corruption
+# noised_x = corrupt(x, amount)
+# # Plotting the noised version
+# axs[1].set_title("Corrupted data (-- amount increases -->)")
+# axs[1].imshow(torchvision.utils.make_grid(noised_x)[0], cmap="Greys")
+# plt.show()
 
-# Plotting the input data
-fig, axs = plt.subplots(2, 1, figsize=(12, 5))
-axs[0].set_title("Input data")
-axs[0].imshow(torchvision.utils.make_grid(x)[0], cmap="Greys")
+
+class BasicUNet(nn.Module):
+    """A minimal UNet implementation."""
+
+    def __init__(self, in_channels=1, out_channels=1):
+        super().__init__()
+        self.down_layers = torch.nn.ModuleList(
+            [
+                nn.Conv2d(in_channels, 32, kernel_size=5, padding=2),
+                nn.Conv2d(32, 64, kernel_size=5, padding=2),
+                nn.Conv2d(64, 64, kernel_size=5, padding=2),
+            ]
+        )
+        self.up_layers = torch.nn.ModuleList(
+            [
+                nn.Conv2d(64, 64, kernel_size=5, padding=2),
+                nn.Conv2d(64, 32, kernel_size=5, padding=2),
+                nn.Conv2d(32, out_channels, kernel_size=5, padding=2),
+            ]
+        )
+        self.act = nn.SiLU()  # The activation function
+        self.downscale = nn.MaxPool2d(2)
+        self.upscale = nn.Upsample(scale_factor=2)
+
+    def forward(self, x):
+        h = []
+        for i, l in enumerate(self.down_layers):
+            x = self.act(l(x))  # Through the layer and the activation function
+            if i < 2:  # For all but the third (final) down layer:
+                h.append(x)  # Storing output for skip connection
+                x = self.downscale(x)  # Downscale ready for the next layer
+
+        for i, l in enumerate(self.up_layers):
+            if i > 0:  # For all except the first up layer
+                x = self.upscale(x)  # Upscale
+                x += h.pop()  # Fetching stored output (skip connection)
+            x = self.act(l(x))  # Through the layer and the activation function
+
+        return x
+
+net = BasicUNet()
+x = torch.rand(8, 1, 28, 28)
+print(net(x).shape)
+print(sum([p.numel() for p in net.parameters()]))
 
 
-# Adding noise
+# Dataloader (you can mess with batch size)
+batch_size = 128
+train_dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+# How many runs through the data should we do?
+n_epochs = 3
+# Create the network
+net = BasicUNet()
+net.to(device)
+# Our loss function
+loss_fn = nn.MSELoss()
+# The optimizer
+opt = torch.optim.Adam(net.parameters(), lr=1e-3)
+# Keeping a record of the losses for later viewing
+losses = []
+
+# The training loop
+for epoch in range(n_epochs):
+    for x, y in train_dataloader:
+        # Get some data and prepare the corrupted version
+        x = x.to(device)  # Data on the GPU
+        noise_amount = torch.rand(x.shape[0]).to(device)  # Pick random noise amounts
+        noisy_x = corrupt(x, noise_amount)  # Create our noisy x
+        # Get the model prediction
+        pred = net(noisy_x)
+        # Calculate the loss
+        loss = loss_fn(pred, x)  # How close is the output to the true 'clean' x?
+        # Backprop and update the params:
+        opt.zero_grad()
+        loss.backward()
+        opt.step()
+        # Store the loss for later
+        losses.append(loss.item())
+
+    # Print our the average of the loss values for this epoch:
+    avg_loss = sum(losses[-len(train_dataloader) :]) / len(train_dataloader)
+    print(f"Finished epoch {epoch}. Average loss for this epoch: {avg_loss:05f}")
+
+# View the loss curve
+plt.plot(losses)
+plt.ylim(0, 0.1)
+plt.show()
+# @markdown Visualizing model predictions on noisy inputs:
+
+# Fetch some data
+x, y = next(iter(train_dataloader))
+x = x[:8]  # Only using the first 8 for easy plotting
+# Corrupt with a range of amounts
 amount = torch.linspace(0, 1, x.shape[0])  # Left to right -> more corruption
 noised_x = corrupt(x, amount)
+# Get the model predictions
+with torch.no_grad():
+    preds = net(noised_x.to(device)).detach().cpu()
+# Plot
+fig, axs = plt.subplots(3, 1, figsize=(12, 7))
+axs[0].set_title("Input data")
+axs[0].imshow(torchvision.utils.make_grid(x)[0].clip(0, 1), cmap="Greys")
+axs[1].set_title("Corrupted data")
+axs[1].imshow(torchvision.utils.make_grid(noised_x)[0].clip(0, 1), cmap="Greys")
+axs[2].set_title("Network Predictions")
+axs[2].imshow(torchvision.utils.make_grid(preds)[0].clip(0, 1), cmap="Greys")
 
-# Plotting the noised version
-axs[1].set_title("Corrupted data (-- amount increases -->)")
-axs[1].imshow(torchvision.utils.make_grid(noised_x)[0], cmap="Greys")
-plt.show()
+
+# @markdown Sampling strategy: Break the process into 5 steps and move 1/5'th of the way there each time:
+n_steps = 5
+x = torch.rand(8, 1, 28, 28).to(device)  # Start from random
+step_history = [x.detach().cpu()]
+pred_output_history = []
+
+for i in range(n_steps):
+    with torch.no_grad():  # No need to track gradients during inference
+        pred = net(x)  # Predict the denoised x0
+    pred_output_history.append(pred.detach().cpu())  # Store model output for plotting
+    mix_factor = 1 / (n_steps - i)  # How much we move towards the prediction
+    x = x * (1 - mix_factor) + pred * mix_factor  # Move part of the way there
+    step_history.append(x.detach().cpu())  # Store step for plotting
+
+fig, axs = plt.subplots(n_steps, 2, figsize=(9, 4), sharex=True)
+axs[0, 0].set_title("x (model input)")
+axs[0, 1].set_title("model prediction")
+for i in range(n_steps):
+    axs[i, 0].imshow(torchvision.utils.make_grid(step_history[i])[0].clip(0, 1), cmap="Greys")
+    axs[i, 1].imshow(torchvision.utils.make_grid(pred_output_history[i])[0].clip(0, 1), cmap="Greys")
